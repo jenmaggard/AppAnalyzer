f810cc7217c5492ab6564f129b5b404e9263f7c0
kvothe89 <faisal.ladhak@gmail.com>
2013-10-19 09:48:35 -0500

    test

---
+++ AppAnalyzer/src/GraphComponent.java
@@ -0,0 +1,12 @@
+/**
+ * Interface for graph component
+ * @author Faisal
+ *
+ */
+
+public interface GraphComponent {
+
+	public void addComponent(GraphComponent e);
+	
+	public void removeComponent(GraphComponent e);
+}
